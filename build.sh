b34c7761467b1edff3ea3ec37d389692c49b67b1
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-06-15 14:05:16 -0400

    More Build Script Oopsies

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-echo Build Script (win32)
-echo (C) 2021 AddyPug Teknologies Incorporated
+echo "Build Script (Linux)"
+echo "(C) 2021 AddyPug Teknologies Incorporated"
 echo Installing...
 npm install && echo Importing... && npm run import && echo Building... && npm run Building
 
