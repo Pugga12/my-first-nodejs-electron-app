4a929bc491b76389d3dc78b48638f542cbe87208
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-06-15 14:19:36 -0400

    lol remember that time when we had to fix the build scripts a ton

--- build-win32.bat
+++ build-win32.bat
@@ -2,6 +2,6 @@
 echo Build Script (win32)
 echo (C) 2021 AddyPug Teknologies Incorporated
 echo Installing...
-npm install && echo Importing... && npm run import && echo Building... && npm run Building
+npm install && echo Importing... && npm run import && echo Building... && npm run build
 
 echo Exiting...
