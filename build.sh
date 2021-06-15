39562c5547dcb78b02b6c6547332528659e82738
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-06-15 13:49:11 -0400

    Complete Build Scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
+@echo off
+echo Build Script (win32)
+echo (C) 2021 AddyPug Teknologies Incorporated
 echo Installing...
-npm install
-echo Importing...
-npm run import
-echo Building...
-npm run build
+npm install && echo Importing... && npm run import && echo Building... && npm run Building
+
+echo Exiting...
