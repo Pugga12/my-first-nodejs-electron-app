5c9052023470caa5f2822bb297510f98fc577186
Pugga12 <64217755+Pugga12@users.noreply.github.com>
2021-06-19 10:33:08 -0400

    Add support message

--- renderer.js
+++ renderer.js
@@ -5,5 +5,11 @@ document.getElementById('toggle-dark-mode').addEventListener('click', async () =
   
   document.getElementById('reset-to-system').addEventListener('click', async () => {
     await window.darkMode.system()
-    document.getElementById('theme-source').innerHTML = 'System'
+    document.getElementById('theme-source').innerHTML = 'Syncing'
   })
+console.timeLog("Sending notification")
+
+const NOTIFICATION_TITLE = 'Show Support'
+const NOTIFICATION_BODY = 'Star this project on GitHub to show your support!'
+new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
+  .onclick = () => open("https://github.com/Pugga12/my-first-nodejs-electron-app")
